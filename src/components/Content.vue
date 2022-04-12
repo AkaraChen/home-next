97ab4762961a10fb775675f2ad39224b7293585a
AkaraChen <akarachen@outlook.com>
2022-04-12 23:41:27 +0800

    Initial commit

---
+++ src/components/Content.vue
@@ -0,0 +1,34 @@
+<template>
+    <p>I am a I'm able to use JavaScript / HTML / CSS / Python / Java to build wonderful sites.</p>
+    <p>I'm a open sourse enthusiast, and I'm pleased to share what I learn.
+        All my works can be seen on <a href="https://github.com/akarachen" target="_blank">Github</a>, and here's <a
+            href="https://blog.akr.moe" target="_blank">my learning note</a> (in Chinese)</p>
+</template>
+
+<style lang="less">
+a {
+    text-decoration: none;
+    color: #121314;
+    position: relative;
+}
+
+a:after {
+    content: '';
+    position: absolute;
+    z-index: -1;
+    top: 80%;
+    left: -0.1em;
+    right: -0.1em;
+    bottom: 0;
+    transition: top 200ms cubic-bezier(0, 0.8, 0.13, 1);
+    background-color: var(--primary-color);
+}
+
+a:hover:after {
+    top: 0%;
+}
+
+p + p {
+    margin-top: -10px;
+}
+</style>
---
+++ src/components/Name.vue
@@ -0,0 +1,33 @@
+<template>
+    <h1>Hi there!</h1>
+    <h2>I'm <span class="me">AkaraChen</span>, a Web <span class="career">Developer</span> from China.</h2>
+</template>
+
+<style scoped lang="less">
+h1 {
+    font-family: var(--title-font);
+    font-size: 2.3rem;
+}
+
+h2 {
+    font-size: 1.5rem;
+    font-family: var(--title-font);
+
+    & .career {
+        background-image: linear-gradient(90deg, #f29914 15%, #0bbbe3 76%);
+        background-clip: text;
+        -webkit-background-clip: text;
+        color: transparent;
+    }
+
+    & .me {
+        background-image: linear-gradient(-20deg, #00cdac 0%, #8ddad5 100%);
+        background-clip: text;
+        -webkit-background-clip: text;
+        color: transparent;
+    }
+}
+</style>
+
+<script lang="ts" setup>
+</script>
